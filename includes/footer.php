d861604cb15438089b36689e6efe83209e0a548b
Gareth Whitley <me@garethwhitley.online>
2021-06-29 11:55:27 +0100

    small copyright change

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row text-center">
         <div class="col">
-        <span class="font-monospace">Copyright &copy; <a href="https://garethwhitley.online">Gareth Whitley</a> 2021</span>
+        <span class="font-monospace">Copyright &copy; 2021 <a href="https://garethwhitley.online">Gareth Whitley</a></span>
         </div>
     </div>
 </div>
